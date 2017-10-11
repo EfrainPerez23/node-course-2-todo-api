e3f713e9f2dad6607ddb6e34e1e4d0a95059a590
Efrain Perez <efrain.perez@stemconnector.com>
2017-10-11 02:53:52 -0400

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -26,6 +26,24 @@ app.get('/todos', (req, res) => {
         res.status(200).send({ todos });
     }, (error) => {
         res.status(400).send(error)
+    });
+
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.status(200).send({ todo });
+        }
+        res.status(404).send();
+    }, (error) => {
+        res.status(400).json(error);
+    }).catch((e) => {
+        res.status(400).send();
     });
 
 });
