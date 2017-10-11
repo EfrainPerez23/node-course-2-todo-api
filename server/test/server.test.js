7b6da418b1b840d742b67752b4651f17331fba87
Efrain Perez <efrain.perez@stemconnector.com>
2017-10-11 01:39:03 -0400

    add test for GET / todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,16 @@ let request = require('supertest');
 var { app } = require('./../server');
 var { Todo } = require('./../models/todo');
 
+
+let todos = [{
+    text: 'first test todo'
+}, {
+    text: 'second test todo'
+}];
 beforeEach((done) => {
-    Todo.remove({}).then(() => { done(); })
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 describe('POST /todos', () => {
     it('should create a new Todo', (done) => {
@@ -22,7 +30,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -42,9 +50,20 @@ describe('POST /todos', () => {
 
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             })
     });
 });
+
+describe('GET / todos', () => {
+    it('should get todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            }).end(done);
+    });
+})
